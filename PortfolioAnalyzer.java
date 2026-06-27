bb3945b12203a0fbd2f7bcfd3fc8687d67be7e25
Tarun Yadav <tarun.yadav42009@gmail.com>
2026-06-27 16:42:13 +0530

    first commit

---
+++ PortfolioAnalyzer.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PortfolioAnalyzer {
+    public static void main(String[] args) {
+       
+        List<Double> spyReturns = Arrays.asList(0.005, -0.002, 0.001, 0.008, -0.004);
+        List<Double> bndReturns = Arrays.asList(0.001, 0.000089, -0.001, 0.002, 0.001);
+        List<Double> gldReturns = Arrays.asList(-0.003, 0.004, 0.0045, -0.001, 0.005);
+
+        // Portfolio weights: 60% SPY, 30% BND, 10% GLD
+        double wSpy = 0.60, wBnd = 0.30, wGld = 0.10;
+        double rfRate = 0.04; // 4% Risk-Free Rate
+
+        List<Double> portfolioReturns = new ArrayList<>();
+
+        // Calculating weighted daily return for the portfolio
+        for (int i = 0; i < spyReturns.size(); i++) {
+            double dailyPtfReturn = (spyReturns.get(i) * wSpy) 
+                                  + (bndReturns.get(i) * wBnd) 
+                                  + (gldReturns.get(i) * wGld);
+            portfolioReturns.add(dailyPtfReturn);
+        }
+
+        //  Metrics
+        double annualizedVol = RiskEngine.calculateVolatility(portfolioReturns);
+        
+        // Mocking a 3-year annualized return of 8% for demonstration
+        double simulatedAnnReturn = 0.08; 
+        double sharpeRatio = RiskEngine.calculateSharpe(simulatedAnnReturn, annualizedVol, rfRate);
+
+        // Report
+        System.out.println("---- BLACKROCK PORTFOLIO RISK REPORT ------");
+        System.out.printf("Annualized Portfolio Volatility: %.2f%%\n", annualizedVol * 100);
+        System.out.printf("Portfolio Sharpe Ratio         : %.2f\n", sharpeRatio);
+        System.out.println("-----------------------");
+    }
+}
---
+++ RiskEngine.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class RiskEngine {
+    
+    // Calculating mean (average) of daily returns
+    public static double calculateMean(List<Double> returns) {
+        double sum = 0;
+        for (double r : returns) {
+            sum += r;
+        }
+        return sum / returns.size();
+    }
+
+    // Calculating Standard Deviation (Volatility)
+    public static double calculateVolatility(List<Double> returns) {
+        double mean = calculateMean(returns);
+        double temp = 0;
+        for (double r : returns) {
+            temp += Math.pow(r - mean, 2);
+        }
+        // standard deviation
+        double dailyVol = Math.sqrt(temp / (returns.size() - 1));
+        
+        return dailyVol * Math.sqrt(252);
+    }
+
+    //Sharpe Ratio
+    public static double calculateSharpe(double annReturn, double annVol, double rfRate) {
+        return (annReturn - rfRate) / annVol;
+    }
+}
